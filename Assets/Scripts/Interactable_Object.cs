12f2db6cd513dc6dc51479bfe8313320a9d1afe7
Banani <249294855+nguyenngohp78-lang@users.noreply.github.com>
2026-08-31 14:04:49 +0700

    Fixed the encapsulation error in Interactable_Object

--- Assets/Scripts/Interactable_Object.cs
+++ Assets/Scripts/Interactable_Object.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 //This is basically just a script that u can put to an Object, and itll be considered an Object that can be interacted with, hence the name Interactable_Object. It has a public string that can be set in the editor, and a public function that returns the string. This is used in the SelectionManager script to display the name of the object when the player looks at it.
 public class Interactable_Object : MonoBehaviour
 {
-    public string ItemName { get; private set; }
-
+    [field: SerializeField] public string ItemName { get; private set; }
     /* So instead of the old C++ looking ahh code
     public string GetItemName()
     {
